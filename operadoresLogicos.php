824cb4bbbf729bdb011ecf7883d78c2ecac74b9f
Yerson Bocanegra Guerrero <yerson.ybg@gmail.com>
2020-05-15 00:21:21 -0500

    Operadores Logicos

---
+++ operadoresLogicos.php
@@ -0,0 +1,18 @@
+<?php
+$a = 3;
+$b = 9;
+if ($a <= 3 && $b != 9) {
+    echo "Se cumple la condición";
+} else {
+
+    echo "No se cumple la condición";
+}
+print("<br>");
+$Dni = true;
+$edad = 18;
+
+if ($edad === 18 || $dni == true) {
+    echo "Bienvenido";
+} else {
+    echo "No puedes ingresar";
+}
