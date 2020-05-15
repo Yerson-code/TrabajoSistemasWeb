289bb0315a8acb4aff0b7c66212d81c0b4f32803
Yerson Bocanegra Guerrero <yerson.ybg@gmail.com>
2020-05-15 00:04:17 -0500

    actualizacion de condicionales

--- CondicionalIf.php
+++ CondicionalIf.php
@@ -1,9 +1,14 @@
 <?php
-$usuario="Alberto";
-$password=123;
+$usuario = "Alberto";
+$password = 123;
 
-if($usuario==="Alberto" && $password===123){
-    echo"Bienvenido al sistema";
-}else{
-    echo"Acceso denegado";
+if ($usuario === "Alberto" && $password === 123) {
+    echo "Bienvenido al sistema";
+} else {
+    echo "Acceso denegado";
 }
+print("</br>");
+print("<h3>Operador Ternario</h3>");
+
+$edad = 18;
+echo ($edad >= 18) ? "Eres mayor de edad" : "Eres menor de edad";
